3a35bcd0ccea88dcc009d2fc34aa6b3d157653f0
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-03-07 22:28:19 -0400

    add route for create tool

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,5 +2,8 @@ import { Router } from "express";
 import { ToolsController } from "./controllers/ToolsController";
 
 const router = Router();
+const toolsController = new ToolsController();
+
+router.post("/tools", toolsController.create);
 
 export { router };
