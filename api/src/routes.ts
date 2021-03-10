b185ea2c44107bc7491e71b1068e9e635d28e02e
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2021-03-09 20:24:20 -0400

    add route for delete tool

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -7,5 +7,6 @@ const toolsController = new ToolsController();
 router.post("/tools", toolsController.create);
 router.get("/", toolsController.show);
 router.get("/tools", toolsController.filter);
+router.delete("/tools/:id", toolsController.delete);
 
 export { router };
